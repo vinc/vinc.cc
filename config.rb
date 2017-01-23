572d42ba42fd83b8f37122db3bc9b50ca2055e62
Vincent Ollivier <v@vinc.cc>
2017-01-23 11:45:17 +0000

    Use separate stylesheet for syntax highlighting

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-activate :syntax, :inline_theme => Rouge::Themes::Base16::Solarized.new
+activate :syntax
 
 activate :deploy do |deploy|
   deploy.deploy_method = :sftp
